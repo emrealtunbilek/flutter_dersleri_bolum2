11872abcabb72291af5a98ad967a062670356e78
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-18 20:47:00 +0300

    drawer ve inkwell kullanımı

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ class FlutterDersleri extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Dersleri',
       theme: ThemeData(
-        fontFamily: 'Genel',
-          primarySwatch: Colors.cyan, accentColor: Colors.orangeAccent),
+          fontFamily: 'Genel',
+          primarySwatch: Colors.amber,
+          accentColor: Colors.orangeAccent),
       home: MyHomePage(),
     );
   }
@@ -20,6 +21,74 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text("emrealtunbilek"),
+                accountEmail: Text("emrealtunbilek@gmail.com"),
+                currentAccountPicture: Image.network(
+                    "https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
+                otherAccountsPictures: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.purple,
+                    child: Text("AK"),
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Text("EA"),
+                  )
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.home),
+                      title: Text("Ana Sayfa"),
+                      trailing: Icon(Icons.chevron_right),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.call),
+                      title: Text("Ara"),
+                      trailing: Icon(Icons.chevron_right),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.account_box),
+                      title: Text("Profil"),
+                      trailing: Icon(Icons.chevron_right),
+                    ),
+                    Divider(),
+                    InkWell(
+                      onTap: (){
+
+                      },
+                      splashColor: Colors.cyan,
+                      child: ListTile(
+                        leading: Icon(Icons.home),
+                        title: Text("Ana Sayfa"),
+                        trailing: Icon(Icons.chevron_right),
+                      ),
+                    ),
+                    AboutListTile(
+                      applicationName: "Flutter Dersleri",
+                      applicationIcon: Icon(Icons.save),
+                      applicationVersion: "2.0",
+                      child: Text("ABOUT US"),
+                      applicationLegalese: "asdasdasd",
+                      icon: Icon(Icons.keyboard),
+                      aboutBoxChildren: <Widget>[
+                        Text("Child 1"),
+                        Text("Child 2"),
+                        Text("Child 3"),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text("Flutter Dersleri Bölüm 2"),
         ),
@@ -63,7 +132,6 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-
             Center(
               child: Text(
                 "Kişisel Font Kullanımı",
